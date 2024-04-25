a41bb869134b05e999d63998904338ca5e1a1497
Danila Kirpichiov <tyzizrn@gmail.com>
2024-04-25 20:41:48 +0300

    add likes

--- react-start/src/components/Course.jsx
+++ react-start/src/components/Course.jsx
@@ -1,9 +1,32 @@
+import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 
 const Course = ({ course }) => {
+  const [liked, setLiked] = useState(false);
+
+  useEffect(() => {
+    const storedLikes = JSON.parse(localStorage.getItem('likes')) || {};
+    setLiked(storedLikes[course.id]);
+  }, [course.id]);
+
+  const handleLike = () => {
+    setLiked(!liked);
+    let storedLikes = JSON.parse(localStorage.getItem('likes')) || {};
+    storedLikes[course.id] = !liked;
+    localStorage.setItem('likes', JSON.stringify(storedLikes));
+  };
+
   return (
-    <Card style={{ width: '18rem' }} key={course.id}>
+    <Card style={{ width: '18rem', position: 'relative' }} key={course.id}>
+      <Button
+        variant="light"
+        style={{ position: 'absolute', top: '10px', right: '10px', zIndex: 1 }}
+        onClick={handleLike}
+      >
+        {liked ? <AiFillHeart /> : <AiOutlineHeart />} {/* Используйте иконки, если хотите */}
+      </Button>
       <Card.Img variant="top" src={course.img} style={{ height: '200px', objectFit: 'cover' }}/>
       <Card.Body>
         <Card.Title>{course.title}</Card.Title>
--- react-start/src/data.js
+++ react-start/src/data.js
@@ -2,6 +2,6 @@ const universityData = {
     imageUrl: 'https://nashperevod.ru/sites/default/files/images/pages/mezhdunarodnyy-universitet-prirody-obshchestva-i-cheloveka-dubna/471251html7857f213.jpg',
     title: 'Dubna University',
     description: 'Dubna University is a unique international educational and research center located in the city of Dubna, Moscow Region, Russia. The university offers a wide range of programs in natural sciences, engineering, humanities, and social sciences, and provides students with opportunities to participate in cutting-edge research projects.',
-  };
-  
+};
+
 export default universityData;
