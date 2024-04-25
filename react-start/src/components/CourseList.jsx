121be06b31905e40f36965b50361a9d90358f938
Rhapsodicmajor <kirpichev.danya@yandex.ru>
2024-04-25 19:21:13 +0300

    add bcast button

--- react-start/src/components/CourseList.jsx
+++ react-start/src/components/CourseList.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import Course from './Course';
 import { Row } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 import courseData from '../course_data.json'
-import './CourseList.scss';
+import '../style.scss';
 
 function CourseList() {
   const courseArray = Object.entries(courseData).map(([key, value]) => ({ ...value, key }));
+  const [visibleCount, setCounter] = useState(1);
+  const handleClick = () => setCounter(visibleCount + 1);
   return (
-    <Row className="g-4 CourseList__cards-center">
-      {courseArray.map((course) => (
+    <>
+    <Row className="g-4 CourseList_cards-center">
+      {courseArray.slice(0,visibleCount).map((course) => (
         <div key={course.key} className="col-auto">
           <Course course={course} />
         </div>
       ))}
     </Row>
+    <div className="d-grid gap-2 col-6 mx-auto">
+        <Button variant="primary" size="lg" onClick={handleClick}>
+          More
+        </Button>
+      </div>
+    </>
   );
 };
 
